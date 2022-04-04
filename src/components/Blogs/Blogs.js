dc395661feb27db4b5f209f60fc3efc0c2126966
shufol islam <shufolislam1255@gmail.com>
2022-04-05 00:06:51 +0600

    1 ans and home page designed

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>hello from blog</h2>
+            <h2>Q1: What is context api? how it works?</h2>
+            <p>Ans: </p>
+            <h2>Q2: What is semantic tag?</h2>
+            <p className='home-p'>Ans: Semantic tag is a special kind of html tag.Because these tags gives details about what they are containing/ provide additional information about that document.Its also good practice to use semantic tag in your code.Example of some semantic tag:span, div, del, h1, abbr etc.</p>
         </div>
     );
 };
